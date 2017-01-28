b4896b3306a925c54cdc0b87d17d130846fb4070
estevetarra <ets1995@hotmail.com>
2017-01-29 00:54:53 +0100

    Was Marc fault.. he is not really sending the file by post https://www.tutorialspoint.com/flask/flask_file_uploading.htm

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -6,12 +6,12 @@ import random
 import re
 import time
 
-from flask import Flask, send_from_directory
-from flask import request, abort
-from flask import Flask, request, redirect, url_for, flash
+from flask import Flask, send_from_directory, request, abort, redirect, url_for, flash, Response
+import pprint
+
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = "./"
+UPLOAD_FOLDER = "./../tmp_img/"
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
@@ -50,8 +50,24 @@ def index(filename):
 
 @app.route('/send_qr',methods=['POST'])
 def send_qr():
-    millis = int(round(time.time() * 1000))
-    return json.dumps({"time_frames": 1000,"data": ["#FF2B2B","#AFDACA","#EFDECD"], "time": millis})
+    if 'file' not in request.files:
+        str = pprint.pformat(request.environ, depth=5)
+        print (str)
+        return Response(str, mimetype="text/text")
+        return 'No file part'
+    else:
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            millis = int(round(time.time() * 1000))
+            return json.dumps({"time_frames": 1000,"data": ["#FF2B2B","#AFDACA","#EFDECD"], "time": millis})
+    return 'something when wrong'
     
 
 @app.route('/set_scenario',methods=['POST'])
@@ -60,4 +76,4 @@ def join_room():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
