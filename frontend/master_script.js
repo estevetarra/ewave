92dc816b453a4ea21426e8700124ddb0538e4eab
laiaaa0 <laia.freixas@gmail.com>
2017-01-29 11:01:04 +0000

    afegir opcio de time between frames

--- frontend/master_script.js
+++ frontend/master_script.js
@@ -11,17 +11,18 @@ function myFunction(){
     var colorprimary = document.getElementById('color1').value;
     var RoomName = document.getElementById('roomname').value;
     var colorsecondary = document.getElementById('color2').value;
-    var framestime = 30;
+    var total_frames = document.getElementById('frames').value;
+    var time_bw_frames =document.getElementById('time_bw_frames').value;
 
     var img_type = document.getElementById('type').value;
 
 
 
-    if (img_type=="Vertical")    resultArray  = generateGraph_version1 (framestime,colorprimary,colorsecondary,image_w,image_h);
-    else  resultArray  = generateGraph_version2 (framestime,colorprimary,colorsecondary,image_w,image_h);
+    if (img_type=="Vertical")    resultArray  = generateGraph_version1 (total_frames ,colorprimary,colorsecondary,image_w,image_h);
+    else  resultArray  = generateGraph_version2 (total_frames ,colorprimary,colorsecondary,image_w,image_h);
     alert ("the form was submitted\n QR size : "+QRSize+"\n QRheight:"+QRheight
         +"\n Distance from first person"+QRdist_first+"\nimage_width:"+image_w+"\nimage height:"+image_h+"\ncolor1"+colorprimary+"\ncolor2"+colorsecondary+
-        "\nroom width : "+room_x+"\nroom length:"+room_y+"\n frames : "+framestime+"\n room name ="+RoomName
+        "\nroom width : "+room_x+"\nroom length:"+room_y+"\n frames : "+total_frames +"\n room name ="+RoomName
 
     );
 
@@ -33,9 +34,10 @@ function myFunction(){
             "image_height":image_h,
             "room_x":room_x,
             "room_y":room_y,
-            "time_frames":framestime,
+            "time_frames":total_frames ,
             "data":resultArray,
-            "name":RoomName
+            "name":RoomName,
+            "time_between_frames":time_bw_frames
             };
     //alert(JSON.stringify(objecteJson));
      
@@ -87,15 +89,15 @@ function myFunction(){
     //xhr.send(data);
 //$.post ("127.0.0.1/set_scenario",objecteJson,function (data,status){	alert ("Data : " +data + "\nStatus: "+status);});
 }
-function generateGraph_version1(time_frames,color1,color2, img_w,img_h){
+function generateGraph_version1(total_frames ,color1,color2, img_w,img_h){
 
 	var imgArray = new Array();
-	for (i=0; i<time_frames; i++){
+	for (i=0; i<total_frames ; i++){
 		imgArray[i] = new Array();
 		for (j=0; j<img_h; j++){
 			imgArray[i][j] = new Array();
 			for (k=0; k<img_w;k++){
-				currentcolor = getColor_version1 (i,time_frames,j,img_h,color1,color2);	
+				currentcolor = getColor_version1 (i,total_frames ,j,img_h,color1,color2);
 			
 				imgArray[i][j][k]= currentcolor;
 			} 
@@ -103,15 +105,15 @@ function generateGraph_version1(time_frames,color1,color2, img_w,img_h){
 	}
  	return imgArray;
 }
-function generateGraph_version2(time_frames,color1,color2,img_w,img_h) {
+function generateGraph_version2(total_frames ,color1,color2,img_w,img_h) {
 
     var imgArray = new Array();
-    for (i=0; i<time_frames; i++){
+    for (i=0; i<total_frames ; i++){
         imgArray[i] = new Array();
         for (j=0; j<img_h; j++){
             imgArray[i][j] = new Array();
             for (k=0; k<img_w;k++){
-                currentcolor = getColor_version2 (i,time_frames,k,img_w,color1,color2);
+                currentcolor = getColor_version2 (i,total_frames ,k,img_w,color1,color2);
 
                 imgArray[i][j][k]= currentcolor;
             }
